ac806bf49056c9df17ceabd6a94cffcca9ac20d3
JuanjoEV <juanjoecheverri48@gmail.com>
2023-05-20 00:34:02 -0500

    Sin errores
    Clase gatos

--- Assets/Scripts/Gatos.cs
+++ Assets/Scripts/Gatos.cs
@@ -3,110 +3,107 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Cat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string Name { get; }
+    public int Happiness { get; set; }
+    public bool HasSpecialAbility { get; }
+    public int SpecialAbilityValue { get; }
+    public bool IsSpecialAbilityActive { get; private set; }
+
+    public Cat(string name, bool hasSpecialAbility, int specialAbilityValue)
     {
-
-        public string Name { get; }
-        public int Happiness { get; set; }
-        public bool HasSpecialAbility { get; }
-        public int SpecialAbilityValue { get; }
-        public bool IsSpecialAbilityActive { get; private set; }
-
-
+        Name = name;
+        Happiness = 0;
+        HasSpecialAbility = hasSpecialAbility;
+        SpecialAbilityValue = specialAbilityValue;
+        IsSpecialAbilityActive = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UseSpecialAbility()
     {
-        public Cat(string name, bool hasSpecialAbility, int specialAbilityValue)
+        if (HasSpecialAbility && !IsSpecialAbilityActive)
         {
-            Name = name;
-            Happiness = 0;
-            Energy = 100;
-            HasSpecialAbility = hasSpecialAbility;
-            SpecialAbilityValue = specialAbilityValue;
-            IsSpecialAbilityActive = false;
-        }
-
-        public void UseSpecialAbility()
-        {
-            if (HasSpecialAbility && !IsSpecialAbilityActive)
-            {
             // Activar la habilidad especial
             IsSpecialAbilityActive = true;
-            }
         }
+    }
+}
 
-        class WhiteCat : Cat
+public class WhiteCat : Cat
+{
+    public void UseSpecialAbility(Player player)
+    {
+        if (HasSpecialAbility)
         {
-            void UseSpecialAbility(Player player)
-            {
-                if (HasSpecialAbility)
-                {
-                    // Obtener el número de gatos blancos en posesión del jugador
-                    int whiteCatCount = player.Cats.Count(cat => cat.Color == "blanco");
+            // Obtener el número de gatos blancos en posesión del jugador
+            int whiteCatCount = player.Cats.FindAll(cat => cat is WhiteCat).Count;
 
-                    // Obtener las monedas correspondientes y agregarlas al saldo del jugador
-                    int coinsEarned = whiteCatCount * 10;
-                    player.Coins += coinsEarned;
-                }
-            }
-
-
+            // Obtener las monedas correspondientes y agregarlas al saldo del jugador
+            int coinsEarned = whiteCatCount * 10;
+            player.Coins += coinsEarned;
         }
+    }
+}
 
+public class GreyCat : Cat
+{
+    // Implementar habilidades especiales para gatos grises si es necesario
+}
 
-        class GreyCat : Cat
+public class BlackCat : Cat
+{
+    private DateTime specialAbilityStartTime;
+
+    public void UseSpecialAbility()
+    {
+        if (HasSpecialAbility)
         {
-
+            // Verificar si la habilidad especial aún está activa
+            if (DateTime.Now.Subtract(specialAbilityStartTime).TotalMinutes <= 5)
+            {
+                // Aplicar multiplicador x2 al oro obtenido
+                GoldMultiplier.Multiplier = 2;
+            }
+            else
+            {
+                // Iniciar la habilidad especial
+                GoldMultiplier.Multiplier = 1;
+                specialAbilityStartTime = DateTime.Now;
+            }
         }
+    }
+}
 
-        class BlackCat : Cat
+public class OrangeCat : Cat
+{
+    private List<Cat> catsInRoom;
+    private System.Threading.Timer specialAbilityTimer;
+
+    public OrangeCat()
+    {
+        catsInRoom = new List<Cat>();
+        specialAbilityTimer = new System.Threading.Timer(ActivateSpecialAbility, null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
+    }
+
+    public void UseSpecialAbility()
+    {
+        if (catsInRoom.Count(c => c is OrangeCat) >= 2)
         {
-            void UseSpecialAbility()
-            {
-                if (HasSpecialAbility)
-                {
-                    // Verificar si la habilidad especial aún está activa
-                    if (DateTime.Now.Subtract(specialAbilityStartTime).TotalMinutes <= 5)
-                    {
-                        // Aplicar multiplicador x2 al oro obtenido
-                        GoldMultiplier.Multiplier = 2;
-                    }
-                    else
-                    {
-                        // Iniciar la habilidad especial
-                        GoldMultiplier.Multiplier = 1;
-                        specialAbilityStartTime = DateTime.Now;
-                    }
-                }
-            }
+            IsSpecialAbilityActive = true;
+            specialAbilityTimer.Change(TimeSpan.FromMinutes(5), TimeSpan.Zero);
         }
-        
-        class OrangeCat : Cat
+        else
         {
-            void UseSpecialAbility()
-            {
-                if (catsInRoom.Count(c => c.Color == "naranja") >= 2)
-                {
-                    IsSpecialAbilityActive = true;
-                    specialAbilityTimer.Change(TimeSpan.FromMinutes(5), TimeSpan.Zero);
-                }
-                else
-                {
-                    IsSpecialAbilityActive = false;
-                    specialAbilityTimer.Change(TimeSpan.Zero, TimeSpan.FromMinutes(5));
-                }
-            }
-
-            void ActivateSpecialAbility(object state)
-            {
-                IsSpecialAbilityActive = false;
-                specialAbilityTimer.Change(TimeSpan.Zero, TimeSpan.FromMinutes(5));
-            }
+            IsSpecialAbilityActive = false;
+            specialAbilityTimer.Change(TimeSpan.Zero, TimeSpan.FromMinutes(5));
         }
+    }
+
+    private void ActivateSpecialAbility(object state)
+    {
+        IsSpecialAbilityActive = false;
+        specialAbilityTimer.Change(TimeSpan.Zero, TimeSpan.FromMinutes(5));
     }
 }
