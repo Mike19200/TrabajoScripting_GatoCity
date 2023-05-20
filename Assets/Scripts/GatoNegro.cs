2fd5e6d95d70fece361bf20adf6eac4c95afb745
DevilQu3enn <109630035+DevilQu3enn@users.noreply.github.com>
2023-05-20 08:27:58 -0500

    GAtoNegroFuncional

---
+++ Assets/Scripts/GatoNegro.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatoNegro : MonoBehaviour
+{
+    private ScriptCoins scriptCoins; // Referencia al script ScriptCoins
+
+    [SerializeField] private float tiempoProduccionExtra = 3600f;
+    [SerializeField] private int cantidadMonedasExtra = 50;
+
+    private float tiempoPasado = 0f;
+    private bool produccionExtraActivada = false;
+
+    private void Start()
+    {
+        // Obtener la referencia al script ScriptCoins en el mismo objeto o en cualquier objeto en la escena
+        scriptCoins = FindObjectOfType<ScriptCoins>();
+
+        // Iniciar la producción extra en el inicio del juego
+        produccionExtraActivada = true;
+    }
+
+    private void Update()
+    {
+        // Actualizar el tiempo pasado
+        tiempoPasado += Time.deltaTime;
+
+        // Si ha pasado el tiempo de producción extra
+        if (tiempoPasado >= tiempoProduccionExtra)
+        {
+            tiempoPasado = 0f;
+
+            // Activar la producción extra
+            produccionExtraActivada = true;
+        }
+
+        // Si la producción extra está activada
+        if (produccionExtraActivada && scriptCoins != null)
+        {
+            // Aumentar la producción de monedas habitación en el script ScriptCoins
+            ScriptCoins.produccionMonedasHabitacion += cantidadMonedasExtra;
+
+            // Actualizar el texto de producción de monedas habitación en el script ScriptCoins
+            if (scriptCoins.textoProduccionMonedasHabitacion != null)
+            {
+                scriptCoins.textoProduccionMonedasHabitacion.text = "GatoCoins: " + ScriptCoins.produccionMonedasHabitacion.ToString();
+            }
+
+            // Reiniciar la producción extra
+            produccionExtraActivada = false;
+        }
+    }
+}
